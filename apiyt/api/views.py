09e16097c03dc90844361b1ce9f2adafe4391b19
KatiaLayi <katialayi@hotmail.com>
2023-03-30 22:16:32 -0300

    Commit inicial

---
+++ apiyt/api/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .models import User, Profile , Permission
+from rest_framework import viewsets,filters
+from .serializers import UserSerializer
+# Create your views here.reate/
+def create(request):
+    x=User(uname='Pato',password='123456',mail='pato@mail.com',rut='2222222')
+    x.save()
+    return render(request, 'create.html')
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    #Sistema de filtros
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['uname']
