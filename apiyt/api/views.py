0592e6702b3e5d55af75042d1aedce884526227d
KatiaLayi <katialayi@hotmail.com>
2023-03-30 22:23:03 -0300

    Test Commit

--- apiyt/api/views.py
+++ apiyt/api/views.py
@@ -4,7 +4,7 @@ from rest_framework import viewsets,filters
 from .serializers import UserSerializer
 # Create your views here.reate/
 def create(request):
-    x=User(uname='Pato',password='123456',mail='pato@mail.com',rut='2222222')
+    x=User(uname='Pat',password='123456',mail='pato@mail.com',rut='2222222')
     x.save()
     return render(request, 'create.html')
 class UserViewSet(viewsets.ModelViewSet):
