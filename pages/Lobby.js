9db814e142c57189746451b28b6641425ac496ee
mantezanar <129032287+mantezanar@users.noreply.github.com>
2023-05-07 19:16:10 -0400

    Update Lobby.js

--- pages/Lobby.js
+++ pages/Lobby.js
@@ -7,24 +7,14 @@ const jwt = require('jsonwebtoken');
 const secretToken = "M+Yidu6bWMk9GKkJopL0Sk+ri/RRcBFTF5DmxvbBZaJj+ouXBWzNeSb0qf+rG0GuLXqeD34vZ0RKH2LnS+0INw=="
 let url ="https://middleware-aos.vercel.app/login"
 
-let sesion_actual = "sesion actual"
-const token_sesion = jwt.sign(sesion_actual, secretToken);
-let config ={
-    method: "POST",
-    headers: {
-        Authorization: `Bearer ${token_sesion}`
-        }
-    }
-fetch(url, config).then((response) => response.json()).then((data) => {
-    const sesion = jwt.verify(data, secretToken)
-    console.log(sesion)
-    if (sesion == "No"){
-        Router.push('/')
-    }else {
-        Router.push('/Lobby')
-    }
+const token_sesion = req.headers.authorization.split(' ')[1];
+const decoded = jwt.verify(token_sesion, secretToken)
 
-    })   
+console.log(decoded)
+
+
+
+
 
 
 
