7db8db1e52511cd52bb45158675394338966a042
mantezanar <129032287+mantezanar@users.noreply.github.com>
2023-05-06 23:23:30 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -41,6 +41,12 @@ export default function Iniciar() {
 
       fetch(url2, config2).then((response) => response.json()).then((data2) => {
         const decoded = jwt.verify(data2, secretToken)
+        if (decoded.aud == authenticated){
+          Router.push('/Lobby')
+          }else {
+          Router.push('/')
+          }
+
         console.log(decoded)
       })
     })
