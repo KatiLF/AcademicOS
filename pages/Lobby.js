ac1dea471bdb739b8a2739e50a056a3b54e8a3af
KatiLF <katialayi@hotmail.com>
2023-05-08 19:54:19 -0400

    Update Lobby.js

--- pages/Lobby.js
+++ pages/Lobby.js
@@ -16,19 +16,7 @@ export default function Lobby() {
     let calculo = 'Calculo'
     let programacion = 'Programacion'
 
-    useEffect(() => {
-        //const sessionToken = Cookies.get('sesion');
-        if (document.cookie.indexOf('sesion') === -1) {
-          // La cookie no existe
-          console.log('la cookie no existe');
-          Router.push('/');
-        } else {
-          // La cookie existe
-          Router.push('/Lobby');
-          console.log('la cookie no existe');
-        }
-      }, []);
-      
+
     function archivos(x){
         let data = x
 
@@ -65,7 +53,7 @@ export default function Lobby() {
                     <li className="nav-item"><a className="nav-link" href="Lobby">Inicio</a></li>
                   <li className="nav-item"><a className="nav-link" href="Recursos">Recursos</a></li>
                   <li className="nav-item"><a className="nav-link" href="acerca">Acerca de mi</a></li>
-                  <li className="nav-item"><a className="nav-link" onClick={() => Cookies.remove('sesion')}>Cerrar Sesión</a></li>
+                  <li className="nav-item"><a className="nav-link" href="Lobby" onClick={() => Cookies.remove('sesion')}>Cerrar Sesión</a></li>
 
             
                     </ul>
