d2b1d1c4f678c13bed4b35d95ec4507643b95d1b
mantezanar <129032287+mantezanar@users.noreply.github.com>
2023-05-08 00:23:42 -0400

    Update Lobby.js

--- pages/Lobby.js
+++ pages/Lobby.js
@@ -2,7 +2,6 @@ import { useState, useEffect } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import styles from '@/styles/Home.module.css'
 import Router from 'next/router'
-import { decoded } from '.'
 const jwt = require('jsonwebtoken');
 const { ChildComponent } = require ('pages/index.js')
 
