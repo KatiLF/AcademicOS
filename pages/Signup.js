0dc7d30c02d4800701d22d0d1dcc2c6f669864ab
mantezanar <129032287+mantezanar@users.noreply.github.com>
2023-05-08 18:51:24 -0400

    Update Signup.js

--- pages/Signup.js
+++ pages/Signup.js
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Router from 'next/router'
 
@@ -34,7 +35,7 @@ export default function Signup() {
     fetch(url, config).then((response) => response.json()).then((data) => {
       console.log(data)
     })  
-    }
+    
   }
 
 
